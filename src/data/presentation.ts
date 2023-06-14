f5d6705b3180cfff7ff021cdb65e3aa6a50c65c0
tidemax <galangadhi47@gmail.com>
2023-06-14 19:19:51 +0700

    message

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -11,8 +11,8 @@ type Presentation = {
 };
 
 const presentation: Presentation = {
-  mail: "maxencewolff.pro@gmail.com",
-  title: "Hi, I’m Maxence 👋",
+  mail: "galangadhi47@gmail.com",
+  title: "Hi, I’m Galang 👋",
   description:
     "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
   socials: [
